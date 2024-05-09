ad552c60ec7906fcf3e07e63900b1018c101328b
0xdev <zinzuvadiyameet98@gmail.com>
2024-05-09 15:09:47 +0530

    user registration function implimented

--- contracts/escrow_contract/src/lib.rs
+++ contracts/escrow_contract/src/lib.rs
@@ -1,12 +1,16 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, contracttype, Env};
+use soroban_sdk::{contract, contractimpl, contracttype, log, symbol_short, Env, Symbol};
+
+#[contracttype]
+pub enum RegisteredUsers {
+    User(Symbol),
+}
 
 #[contracttype]
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub enum ProjectStatus {
-    Active,
-    Completed,
-    Disputed,
+pub struct User {
+    pub user_id: u64,
+    pub user_name: Symbol,
 }
 
 #[contract]
@@ -14,19 +18,50 @@ pub struct EscrowContract;
 
 #[contractimpl]
 impl EscrowContract {
-    pub fn create_project(e: Env, _total_budget: u32) {}
+    pub fn register_user(e: Env, _user_name: Symbol) -> User {
+        let mut user: User = Self::view_user(e.clone(), _user_name.clone());
 
-    pub fn fund_project() {}
+        if user.user_id != 0 {
+            panic!("User already exists");
+        } else {
+            user.user_id = e.ledger().timestamp();
+            user.user_name = _user_name.clone();
 
-    pub fn accept_project() {}
+            e.storage()
+                .persistent()
+                .set(&RegisteredUsers::User(_user_name.clone()), &user);
+        }
 
-    pub fn submit_milestone() {}
+        log!(&e, "User Registered: {}", _user_name);
 
-    pub fn accept_milestone() {}
+        user
+    }
 
-    pub fn release_fund() {}
+    pub fn view_user(e: Env, _user_name: Symbol) -> User {
+        let key: RegisteredUsers = RegisteredUsers::User(_user_name.clone());
+        e.storage().instance().get(&key).unwrap_or(User {
+            user_id: 0,
+            user_name: symbol_short!("not_found"),
+        })
+    }
 
-    pub fn raise_dispute() {}
+    pub fn create_project(e: Env, _total_budget: u32) {
+        log!(&e, "Projects Total Budget: {}", _total_budget);
+    }
 
-    pub fn resolve_dispute() {}
+    // pub fn fund_project() {}
+
+    // pub fn accept_project() {}
+
+    // pub fn submit_milestone() {}
+
+    // pub fn accept_milestone() {}
+
+    // pub fn release_fund() {}
+
+    // pub fn raise_dispute() {}
+
+    // pub fn resolve_dispute() {}
 }
+
+mod test;
--- contracts/escrow_contract/src/test.rs
+++ contracts/escrow_contract/src/test.rs
@@ -1,17 +1,27 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{symbol_short, vec, Env};
+use soroban_sdk::Env;
 
 #[test]
 fn test() {
     let env = Env::default();
-    let contract_id = env.register_contract(None, HelloContract);
-    let client = HelloContractClient::new(&env, &contract_id);
+    let contract_id = env.register_contract(None, EscrowContract);
+    let client = EscrowContractClient::new(&env, &contract_id);
 
-    let words = client.hello(&symbol_short!("Dev"));
     assert_eq!(
-        words,
-        vec![&env, symbol_short!("Hello"), symbol_short!("Dev"),]
+        client.register_user(&symbol_short!("john")),
+        User {
+            user_id: env.ledger().timestamp(),
+            user_name: symbol_short!("john"),
+        }
+    );
+
+    assert_eq!(
+        client.register_user(&symbol_short!("bob")),
+        User {
+            user_id: env.ledger().timestamp(),
+            user_name: symbol_short!("bob"),
+        }
     );
 }
